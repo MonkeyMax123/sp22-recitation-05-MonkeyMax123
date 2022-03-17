c45395cece5013e4a6a571d2937133b103cadf14
MonkeyMax123 <98365388+MonkeyMax123@users.noreply.github.com>
2022-03-17 09:27:36 -0500

    Ready For submission

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
 
+
 def supersort(a, k):
     """
     The main sorting algorithm. You'll complete the
     three funcions count_values, get_positions, and construct_output.
-    
+
     Params:
       a.....the input list
       k.....the maximum element in a
-      
+
     Returns:
       sorted version a
     """
@@ -16,25 +17,30 @@ def supersort(a, k):
     positions = get_positions(counts)
     return construct_output(a, positions)
 
+
 def count_values(a, k):
     """
     Params:
       a.....input list
       k.....maximum value in a
-      
+
     Returns:
       a list of k values; element i of the list indicates
       how often value i appears in a
-      
+
     >>> count_values([2,2,1,0,1,0,1,3], 3)
     [2, 3, 2, 1]
     """
-    ###TODO
-    pass
+    result = [0] * (k + 1)
+    for number in a:
+        result[number] += 1
+    return result
+
 
 def test_count_values():
-    assert count_values([2,2,1,0,1,0,1,3], 3) == [2, 3, 2, 1]
-    
+    assert count_values([2, 2, 1, 0, 1, 0, 1, 3], 3) == [2, 3, 2, 1]
+
+
 def get_positions(counts):
     """
     Params:
@@ -44,14 +50,17 @@ def get_positions(counts):
       appearance of i in the desired output.
 
     >>> get_positions([2, 3, 2, 1])
-    [0, 2, 5, 7]    
+    [0, 2, 5, 7]
     """
-    ###TODO
-    pass
-    
+    p = scan(plus, 0, counts)
+    p = [0] + p[0][:-1]
+    return p
+
+
 def test_get_positions():
     assert get_positions([2, 3, 2, 1]) == [0, 2, 5, 7]
-    
+
+
 def construct_output(a, positions):
     """
     Construct the final, sorted output.
@@ -59,19 +68,24 @@ def construct_output(a, positions):
     Params:
       a...........input list
       positions...list of first location of each value in the output.
-      
+
     Returns:
       sorted version of a
 
     >>> construct_output([2,2,1,0,1,0,1,3], [0, 2, 5, 7])
-    [0,0,1,1,1,2,2,3]    
+    [0,0,1,1,1,2,2,3]
     """
-    ###TODO
-    pass
+    res = [0] * len(a)
+    for i in a:
+        res[positions[i]] = i
+        positions[i] += 1
+    return res
+
 
 def test_construct_output():
-    assert construct_output([2,2,1,0,1,0,1,3], [0, 2, 5, 7]) == [0,0,1,1,1,2,2,3]
-    
+    assert construct_output([2, 2, 1, 0, 1, 0, 1, 3], [0, 2, 5, 7]) == [0, 0, 1, 1, 1, 2, 2, 3]
+
+
 def count_values_mr(a, k):
     """
     Use map-reduce to implement count_values.
@@ -79,46 +93,50 @@ def count_values_mr(a, k):
     """
     # done.
     int2count = dict(run_map_reduce(count_map, count_reduce, a))
-    return [int2count.get(i,0) for i in range(k+1)]
+    return [int2count.get(i, 0) for i in range(k + 1)]
+
 
 def test_count_values_mr():
-    assert count_values_mr([2,2,1,0,1,0,1,3], 3) == [2, 3, 2, 1]
+    assert count_values_mr([2, 2, 1, 0, 1, 0, 1, 3], 3) == [2, 3, 2, 1]
+
 
 def count_map(value):
-    ###TODO
-    pass
+    return [(value, 1)]
+
 
 def count_reduce(group):
-    ###TODO
-    pass
+    return tuple((group[0], reduce(plus, 0, group[1])))
 
 
 # the below functions are provided for use above.
 
 def run_map_reduce(map_f, reduce_f, mylist):
-    # done. 
+    # done.
     pairs = flatten(list(map(map_f, mylist)))
     groups = collect(pairs)
     return [reduce_f(g) for g in groups]
 
+
 def collect(pairs):
-    # done.     
+    # done.
     result = defaultdict(list)
     for pair in sorted(pairs):
         result[pair[0]].append(pair[1])
     return list(result.items())
 
-def plus(x,y):
-    # done. 
+
+def plus(x, y):
+    # done.
     return x + y
 
 
 def scan(f, id_, a):
-    # done. 
+    # done.
     return (
-            [reduce(f, id_, a[:i+1]) for i in range(len(a))],
-             reduce(f, id_, a)
-           )
+        [reduce(f, id_, a[:i + 1]) for i in range(len(a))],
+        reduce(f, id_, a)
+    )
+
 
 def reduce(f, id_, a):
     # done. do not change me.
@@ -127,15 +145,23 @@ def reduce(f, id_, a):
     elif len(a) == 1:
         return a[0]
     else:
-        return f(reduce(f, id_, a[:len(a)//2]),
-                 reduce(f, id_, a[len(a)//2:]))
-    
+        return f(reduce(f, id_, a[:len(a) // 2]),
+                 reduce(f, id_, a[len(a) // 2:]))
+
+
 def iterate(f, x, a):
     # done. do not change me.
     if len(a) == 0:
         return x
     else:
         return iterate(f, f(x, a[0]), a[1:])
-    
+
+
 def flatten(sequences):
     return iterate(plus, [], sequences)
+
+
+test_count_values_mr()
+test_count_values()
+test_construct_output()
+test_get_positions()
